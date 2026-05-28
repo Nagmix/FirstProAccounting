0dd2235b538e9dd1dc7c0b49f86c03cc361316e3
Z User <z@container>
2026-05-28 17:15:58 +0000

    fix: Fix l10n import for Flutter 3.44+ - use local output dir instead of synthetic package
    - Change l10n.yaml to output to lib/l10n/generated/ instead of synthetic flutter_gen package
    - Add synthetic-package: false to l10n.yaml (Flutter 3.44+ deprecates synthetic packages)
    - Update import in main.dart from package:flutter_gen to relative path
    - This resolves the 'Couldn't resolve package flutter_gen' build error on CI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'l10n/generated/app_localizations.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'core/constants/app_constants.dart';
