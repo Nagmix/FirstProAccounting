07d1d6d3182bb3931e8da29402095730b03eecb3
Nagmix <nooraldeen.ahmed@nagmix.net>
2026-06-07 03:39:46 +0000

    fix: restore relative imports in costing_engine_service.dart

--- lib/data/datasources/services/costing_engine_service.dart
+++ lib/data/datasources/services/costing_engine_service.dart
@@ -1,8 +1,8 @@
 import 'package:sqflite_sqlcipher/sqflite.dart';
 
-import 'package:pro_accounting/core/utils/money_helper.dart';
-import 'package:pro_accounting/data/models/inventory_cost_layer_model.dart';
-import 'package:pro_accounting/data/datasources/database_helper.dart';
+import '../../../core/utils/money_helper.dart';
+import '../../models/inventory_cost_layer_model.dart';
+import '../database_helper.dart';
 
 class CostingEngineService {
   final DatabaseHelper _dbHelper;
