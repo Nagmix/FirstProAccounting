7d28ce044227d2337dc904a96a7a71a66f95a90f
Nagmix <2.80517956e+08+Nagmix@users.noreply.github.com>
2026-08-26 04:57:24 +0000

    test: scroll onboarding submit button into viewport

--- test/widget/onboarding_screen_test.dart
+++ test/widget/onboarding_screen_test.dart
@@ -56,8 +56,11 @@ void main() {
     );
 
     final continueButton = find.widgetWithText(ElevatedButton, 'متابعة');
-    await tester.ensureVisible(continueButton);
-    await tester.pumpAndSettle();
+    await tester.scrollUntilVisible(
+      continueButton,
+      300,
+      scrollable: find.byType(SingleChildScrollView),
+    );
     await tester.tap(continueButton);
     await tester.pump();
 
