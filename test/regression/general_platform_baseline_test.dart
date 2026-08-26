dd466ac5cb689be2d351e40328fc0306c4128c84
Nagmix <2.80517956e+08+Nagmix@users.noreply.github.com>
2026-08-26 02:41:44 +0000

    fix: use flutter test baseline import

--- test/regression/general_platform_baseline_test.dart
+++ test/regression/general_platform_baseline_test.dart
@@ -1,6 +1,6 @@
 import 'package:firstpro/data/datasources/migrations/schema.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   setUpAll(() {
